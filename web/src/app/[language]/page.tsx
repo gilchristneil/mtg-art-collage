32ca446840e65dbb37ee732b94317fd7c3b3044b
Neil Gilchrist <neil.g.gilchrist@gmail.com>
2024-11-13 13:50:00 -0500

    removed bloat from boilerplate

--- web/src/app/[language]/page.tsx
+++ web/src/app/[language]/page.tsx
@@ -38,7 +38,7 @@ export default async function Home({ params }: Props) {
             maxItems={6}
           />
         </Grid>
-        <Grid item size={6}>
+        <Grid size={6}>
           <ArtSearchCanvas />
         </Grid>
       </Grid>
